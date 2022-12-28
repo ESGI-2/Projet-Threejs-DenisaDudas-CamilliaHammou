baffe78fdef81885d6aa89ee65b4a69484eac7b6
denisadudas <dudasdenisandrea@yahoo.ro>
2022-12-28 09:14:55 +0100

    ajout soleil sans lumiere

--- js/app.js
+++ js/app.js
@@ -26,36 +26,50 @@ function InitScene() {
 
     // Initialisation et placement de la camera
     _camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000)
-    _camera.position.set(0, 2, 10)
+    _camera.position.set(100, 20, 10)
 
     // Création du controle camera
     _control = new OrbitControls(_camera, _renderer.domElement)
     _control.enableDamping = true;
     _control.dampingFactor = 0.1;
     _control.enablePan = false
-     
+
     //gridhelper
     const gridHelper = new THREE.GridHelper(100, 10)
     _scene.add(gridHelper)
-    
+
      // Création d'une lumiere ambiante
      const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.20)
      _scene.add(ambientLight)
- 
+
+     // Création d'une point light
+     //const pointLight = new THREE.PointLight( 0xff0000, 0.5, 0 );
+     //pointLight.position.set( 0, 0, 0 );
+     //_scene.add( pointLight );
+
      // Création d'une lumiere directionelle
      const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1)
      directionalLight.position.set(-10, 10, 0)
      _scene.add(directionalLight)
- 
+
     //skydome
     const textureloader = new THREE.TextureLoader();
     const bgGeometry = new THREE.SphereGeometry(400, 40, 40);
     const bgMaterial = new THREE.MeshStandardMaterial({
-        map: textureloader.load("./assets/textures/space-background.jpg"), 
+        map: textureloader.load("./assets/textures/space-background.jpg"),
         side: THREE.DoubleSide
     })
     const bg = new THREE.Mesh(bgGeometry, bgMaterial);
     _scene.add(bg);
+
+    //! Soleil
+    const sunTexture = new THREE.TextureLoader()
+    const sunGeometry = new THREE.SphereGeometry(30, 30, 30)
+    const sunMaterial = new THREE.MeshStandardMaterial({
+        map: sunTexture.load("./assets/textures/sun-texture.jpg"),
+    });
+    const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial)
+    _scene.add(sunMesh)
 
 }
 
